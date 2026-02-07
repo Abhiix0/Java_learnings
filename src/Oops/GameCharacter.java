b92d9a60100726fbb779d6af1112fdd6d5f203b7
Abhiix0 <24r21a6778@mlrit.ac.in>
2026-02-07 10:30:43 +0530

    inheritance

--- src/Oops/GameCharacter.java
+++ src/Oops/GameCharacter.java
@@ -46,17 +46,41 @@ public class GameCharacter {
         System.out.println("--------------------");
     }
 
+    static class Mage extends GameCharacter {
+
+        Mage(String name, int health) {
+            super(name, health, "Magic");
+        }
+
+        void castSpell() {
+            System.out.println(getName() + " casts a spell!");
+        }
+
+        @Override
+        public void attack() {
+            setHealth(getHealth() - 100); // mage loses less health
+            System.out.println(getName() + " casts a fireball 🪄");
+        }
+
+    }
+
+
     public static void main(String[] args){
         GameCharacter gc1 = new GameCharacter("AbhiX0",2000,"Time control");
         GameCharacter gc2 = new GameCharacter("Reaperx1",3000,"Instant Death");
-
+        Mage gc3 = new Mage("Luciferx2",2433);
         System.out.println("your characters");
         gc1.showStatus();
         gc2.showStatus();
+        gc3.showStatus();
         System.out.println("A wild beast appears and attacks "+ gc2.getName());
         gc1.attack();
         gc2.heal();
         System.out.println(gc2.getName() + " finishes the beast with " + gc2.getPower());
-
+        System.out.println("wild Beasts appears");
+        gc3.castSpell();
+        System.out.println("One beast Left");
+        gc3.attack();
+        System.out.println("Wild beasts Died");
     }
 }
