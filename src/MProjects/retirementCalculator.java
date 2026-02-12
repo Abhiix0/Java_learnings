47be147a0246fd8e4fa0d51d88c3365ba35e1d7d
Abhiix0 <24r21a6778@mlrit.ac.in>
2026-02-12 13:42:21 +0530

    retirement calculator

---
+++ src/MProjects/retirementCalculator.java
@@ -0,0 +1,19 @@
+package MProjects;
+
+import java.time.Year;
+import java.util.Scanner;
+public class retirementCalculator {
+    public void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your current age? ");
+        int currentAge = sc.nextInt();
+        System.out.println("At What age would you like to retire? ");
+        int retireAge = sc.nextInt();
+        int left = retireAge - currentAge;
+        System.out.println("You have " + left + " years left until you can retire." );
+        int currentYear = Year.now().getValue();
+        int retiredYear = currentYear + left;
+        System.out.println("it's "+ currentYear +", so you can retire in "+ retiredYear);
+        sc.close();
+    }
+}
